efc27b016aa22a67593f5754a6a9a56ed7c02f81
David Supplee <dwsupplee@gmail.com>
2018-03-23 09:45:41 -0400

    Restructure apis (#943)
    * Manual changes for restructure
    
    * Restructure APIs via script
    
    * Fix Debugger snippets
    
    * Add github PR templates
    
    * Initial fixes for subrepos
    
    * Fix Core system tests
    
    * Update BigQuery system tests
    
    * Changes for Spanner
    
    * Update phpunit files
    
    * Update bootstrap files
    
    * Datastore updates
    
    * Add keyfile stub files
    
    * Updates for Firestore
    
    * Fix Language composer
    
    * Fix logging tests and composer
    
    * Fix pubsub composer and unit tests
    
    * Update Speech composer
    
    * Fix composer and tests for Storage
    
    * Update Trace composer
    
    * Update Translate composer
    
    * Update Vision composer
    
    * Update gapic only APIs
    
    * Update composer files
    
    * Rename test folders
    
    * Move tests and update namespaces
    
    * Update phpunit files
    
    * Update Core
    
    * Update doc generator to work with API restructure.
    
    * Changes after merging from master
    
    * Update after merge
    
    * Small fixes for docs
    
    * Update dev CLI for API restructure
    
    * Updates after merge, update Snippet namespaces
    
    * Move conformance and perf
    
    * Update bootstrap files
    
    * Update bootstrap
    
    * Address PR comments
    
    * Restore snippet test
    
    * Remove keyfile stubs
    
    * Update pubsub dependencies
    
    * Update composer, move debugger bin
    
    * Remove references to local Core
    
    * Update bootstrap files
    
    * Update docs
    
    * Update core to pass doc gen
    
    * Fix doc generator
    
    * Add daemonutils to locate autoloader
    
    * Remove DaemonUtils, update autoloader locator
    
    * Restructure testing (#929)
    
    * Add system test credentials CLI
    
    * Update Snippet testing for restructure
    
    * Fix unit tests for restructure
    
    * Fix system test runner
    
    * Fix conformance bootstrap
    
    * fix credentials CLIts
    
    * Fix docs
    
    * exclude dev folder from snippet parsing
    
    * install latest composer
    
    * Credentials CLI should not throw exception
    
    * Test component integration
    
    * Don't fail if extension is missing
    
    * skip tests if debugger ext is missing
    
    * Allow setting the search location paths for the SourceLocationResolver (#6)
    
    * update bin paths
    
    * move dlp v2 to new structure
    
    * require opis/closure for tests
    
    * add internal tags and fix cs
    
    * update configurations
    
    * resolve issues with perf/conformance tests
    
    * update bootstraps
    
    * update PR template
    
    * show bigtable on docs
    
    * fix doc generation

---
+++ README.md
@@ -0,0 +1,22 @@
+# Google Cloud PHP Vision
+
+> Idiomatic PHP client for [Cloud Vision](https://cloud.google.com/vision/).
+
+[![Latest Stable Version](https://poser.pugx.org/google/cloud-vision/v/stable)](https://packagist.org/packages/google/cloud-vision) [![Packagist](https://img.shields.io/packagist/dm/google/cloud-vision.svg)](https://packagist.org/packages/google/cloud-vision)
+
+* [Homepage](http://googlecloudplatform.github.io/google-cloud-php)
+* [API documentation](http://googlecloudplatform.github.io/google-cloud-php/#/docs/cloud-vision/latest/vision/visionclient)
+
+**NOTE:** This repository is part of [Google Cloud PHP](https://github.com/googlecloudplatform/google-cloud-php). Any
+support requests, bug reports, or development contributions should be directed to
+that project.
+
+Use of the included generated clients requires installation of the gRPC PHP extension. For instructions, [see here](https://cloud.google.com/php/grpc).
+
+NOTE: In addition to the gRPC extension, we recommend installing the protobuf extension for improved performance. For installation instructions, [see here](https://cloud.google.com/php/grpc#install_the_protobuf_runtime_library).
+
+## Installation
+
+```
+$ composer require google/cloud-vision
+```
---
+++ src/V1/Gapic/ImageAnnotatorGapicClient.php
@@ -0,0 +1,216 @@
+<?php
+/*
+ * Copyright 2017 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was generated from the file
+ * https://github.com/google/googleapis/blob/master/google/cloud/vision/v1/image_annotator.proto
+ * and updates to that file get reflected here through a refresh process.
+ *
+ * EXPERIMENTAL: This client library class has not yet been declared GA (1.0). This means that
+ * even though we intend the surface to be stable, we may make backwards incompatible changes
+ * if necessary.
+ *
+ * @experimental
+ */
+
+namespace Google\Cloud\Vision\V1\Gapic;
+
+use Google\ApiCore\ApiException;
+use Google\ApiCore\Call;
+use Google\ApiCore\GapicClientTrait;
+use Google\ApiCore\RetrySettings;
+use Google\ApiCore\Transport\TransportInterface;
+use Google\Auth\CredentialsLoader;
+use Google\Cloud\Vision\V1\AnnotateImageRequest;
+use Google\Cloud\Vision\V1\BatchAnnotateImagesRequest;
+use Google\Cloud\Vision\V1\BatchAnnotateImagesResponse;
+use Grpc\Channel;
+use Grpc\ChannelCredentials;
+
+/**
+ * Service Description: Service that performs Google Cloud Vision API detection tasks over client
+ * images, such as face, landmark, logo, label, and text detection. The
+ * ImageAnnotator service returns detected entities from the images.
+ *
+ * EXPERIMENTAL: This client library class has not yet been declared GA (1.0). This means that
+ * even though we intend the surface to be stable, we may make backwards incompatible changes
+ * if necessary.
+ *
+ * This class provides the ability to make remote calls to the backing service through method
+ * calls that map to API methods. Sample code to get started:
+ *
+ * ```
+ * $imageAnnotatorClient = new ImageAnnotatorClient();
+ * try {
+ *     $requests = [];
+ *     $response = $imageAnnotatorClient->batchAnnotateImages($requests);
+ * } finally {
+ *     $imageAnnotatorClient->close();
+ * }
+ * ```
+ *
+ * @experimental
+ */
+class ImageAnnotatorGapicClient
+{
+    use GapicClientTrait;
+
+    /**
+     * The name of the service.
+     */
+    const SERVICE_NAME = 'google.cloud.vision.v1.ImageAnnotator';
+
+    /**
+     * The default address of the service.
+     */
+    const SERVICE_ADDRESS = 'vision.googleapis.com';
+
+    /**
+     * The default port of the service.
+     */
+    const DEFAULT_SERVICE_PORT = 443;
+
+    /**
+     * The name of the code generator, to be included in the agent header.
+     */
+    const CODEGEN_NAME = 'gapic';
+
+    /**
+     * The code generator version, to be included in the agent header.
+     */
+    const CODEGEN_VERSION = '0.0.5';
+
+    private static function getClientDefaults()
+    {
+        return [
+            'serviceName' => self::SERVICE_NAME,
+            'serviceAddress' => self::SERVICE_ADDRESS,
+            'port' => self::DEFAULT_SERVICE_PORT,
+            'scopes' => [
+                'https://www.googleapis.com/auth/cloud-platform',
+                'https://www.googleapis.com/auth/cloud-vision',
+            ],
+            'clientConfigPath' => __DIR__.'/../resources/image_annotator_client_config.json',
+            'restClientConfigPath' => __DIR__.'/../resources/image_annotator_rest_client_config.php',
+            'descriptorsConfigPath' => __DIR__.'/../resources/image_annotator_descriptor_config.php',
+            'versionFile' => __DIR__.'/../../VERSION',
+        ];
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param array $options {
+     *                       Optional. Options for configuring the service API wrapper.
+     *
+     *     @type string $serviceAddress The domain name of the API remote host.
+     *                                  Default 'vision.googleapis.com'.
+     *     @type mixed $port The port on which to connect to the remote host. Default 443.
+     *     @type Channel $channel
+     *           A `Channel` object. If not specified, a channel will be constructed.
+     *           NOTE: This option is only valid when utilizing the gRPC transport.
+     *     @type ChannelCredentials $sslCreds
+     *           A `ChannelCredentials` object for use with an SSL-enabled channel.
+     *           Default: a credentials object returned from
+     *           \Grpc\ChannelCredentials::createSsl().
+     *           NOTE: This option is only valid when utilizing the gRPC transport. Also, if the $channel
+     *           optional argument is specified, then this argument is unused.
+     *     @type bool $forceNewChannel
+     *           If true, this forces gRPC to create a new channel instead of using a persistent channel.
+     *           Defaults to false.
+     *           NOTE: This option is only valid when utilizing the gRPC transport. Also, if the $channel
+     *           optional argument is specified, then this option is unused.
+     *     @type CredentialsLoader $credentialsLoader
+     *           A CredentialsLoader object created using the Google\Auth library.
+     *     @type string[] $scopes A string array of scopes to use when acquiring credentials.
+     *                          Defaults to the scopes for the Google Cloud Vision API.
+     *     @type string $clientConfigPath
+     *           Path to a JSON file containing client method configuration, including retry settings.
+     *           Specify this setting to specify the retry behavior of all methods on the client.
+     *           By default this settings points to the default client config file, which is provided
+     *           in the resources folder. The retry settings provided in this option can be overridden
+     *           by settings in $retryingOverride
+     *     @type array $retryingOverride
+     *           An associative array in which the keys are method names (e.g. 'createFoo'), and
+     *           the values are retry settings to use for that method. The retry settings for each
+     *           method can be a {@see Google\ApiCore\RetrySettings} object, or an associative array
+     *           of retry settings parameters. See the documentation on {@see Google\ApiCore\RetrySettings}
+     *           for example usage. Passing a value of null is equivalent to a value of
+     *           ['retriesEnabled' => false]. Retry settings provided in this setting override the
+     *           settings in $clientConfigPath.
+     *     @type callable $authHttpHandler A handler used to deliver PSR-7 requests specifically
+     *           for authentication. Should match a signature of
+     *           `function (RequestInterface $request, array $options) : ResponseInterface`.
+     *     @type callable $httpHandler A handler used to deliver PSR-7 requests. Should match a
+     *           signature of `function (RequestInterface $request, array $options) : PromiseInterface`.
+     *           NOTE: This option is only valid when utilizing the REST transport.
+     *     @type string|TransportInterface $transport The transport used for executing network
+     *           requests. May be either the string `rest` or `grpc`. Additionally, it is possible
+     *           to pass in an already instantiated transport. Defaults to `grpc` if gRPC support is
+     *           detected on the system.
+     * }
+     * @experimental
+     */
+    public function __construct($options = [])
+    {
+        $this->setClientOptions($options + self::getClientDefaults());
+    }
+
+    /**
+     * Run image detection and annotation for a batch of images.
+     *
+     * Sample code:
+     * ```
+     * $imageAnnotatorClient = new ImageAnnotatorClient();
+     * try {
+     *     $requests = [];
+     *     $response = $imageAnnotatorClient->batchAnnotateImages($requests);
+     * } finally {
+     *     $imageAnnotatorClient->close();
+     * }
+     * ```
+     *
+     * @param AnnotateImageRequest[] $requests     Individual image annotation requests for this batch.
+     * @param array                  $optionalArgs {
+     *                                             Optional.
+     *
+     *     @type RetrySettings|array $retrySettings
+     *          Retry settings to use for this call. Can be a
+     *          {@see Google\ApiCore\RetrySettings} object, or an associative array
+     *          of retry settings parameters. See the documentation on
+     *          {@see Google\ApiCore\RetrySettings} for example usage.
+     * }
+     *
+     * @return \Google\Cloud\Vision\V1\BatchAnnotateImagesResponse
+     *
+     * @throws ApiException if the remote call fails
+     * @experimental
+     */
+    public function batchAnnotateImages($requests, $optionalArgs = [])
+    {
+        $request = new BatchAnnotateImagesRequest();
+        $request->setRequests($requests);
+
+        return $this->startCall(
+            'BatchAnnotateImages',
+            BatchAnnotateImagesResponse::class,
+            $optionalArgs,
+            $request
+        )->wait();
+    }
+}
